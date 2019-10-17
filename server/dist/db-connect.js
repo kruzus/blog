dc7fef0f0d2bb105f807de3cb15a8d28e7ed402e
Bek <kruzus@live.com>
2019-10-17 04:02:22 -0400

    added dotenv

--- server/dist/db-connect.js
+++ server/dist/db-connect.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = require("mysql2");
+require('dotenv').config();
 exports.connection = mysql2_1.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'blog'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
 });
 exports.connection.connect();
 //# sourceMappingURL=db-connect.js.map
--- server/src/db-connect.ts
+++ server/src/db-connect.ts
@@ -1,10 +1,12 @@
 import { createConnection } from 'mysql2';
+require('dotenv').config();
+
 
 export const connection = createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'blog'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
 });
 
 connection.connect();
