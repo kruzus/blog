4bcccdb32bfce1926b75919f7e9ce2604872647a
Bek <kruzus@live.com>
2019-10-27 21:03:51 -0400

    test

--- view/src/components/Blog-container/Blog.js
+++ view/src/components/Blog-container/Blog.js
@@ -1,76 +1,43 @@
-import React, { Component } from 'react';
-import './Blog.scss';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import "./Blog.scss";
+import axios from "axios";
 
 export default class Blog extends Component {
- 
-
-// state = {
-// posts: []
-// };
-
-// componentDidMount() {
-//   axios.get('http://localhost:8000/posts')
-//   .then(res => {
-//     const data = res.data;
-//     this.setState([data]);
-    
-//   console.log(data)
-//   })
-// }
-
-// render(){
-//   return(
-//     //
-//     <div className="blog"> 
-    
-//     <ul>
-//        {console.log(this.state.posts)}
-//       </ul>
-//     </div>
-//   )
-// }
-// }
+  state = {
+    posts: []
+  };
 
 
+  componentDidMount() {
+    axios.get(`http://localhost:8000/posts`)
+      .then(data => {
+      const posts = data.data;
+      this.setState({posts});
+      console.log(posts);
+    });
+  }
 
 
-state = {
-  posts: []
-}
+  render() {
+    const ViewPosts = (props) => {
+      return(
+        <table>
+        <tbody>
+        <tr>
+        <td>Title</td>
+        <td>Body</td>
+        </tr>
+        <tr>
+        <td>{jsx to map over posts for title}</td>
+        <td>{jsx to map over posts for body}</td>
+        </tr>
+        </tbody>
+        </table>
+      )
+    }
 
-componentDidMount() {
-  axios.get(`http://localhost:8000/posts`)
-    .then(res => {
-      const posts = res.data.data;
-      this.setState({ posts });
-    })
-}
-
-render() {
-  return (
-   <div className="blog">
-    
-{/*        
-        {this.state.posts.map(post => {post.title})}
-        {this.state.posts.map(post => {post.body})}
-         */}
-
-
-<table>
-  <tr>
-    <th>Title</th>
-    <th>Body</th>
-  </tr>
-  <tr>
-    <td> {this.state.posts.map(post => <h3>{post.title}</h3>)} </td>
-    <td>{this.state.posts.map(post => <h3>{post.body}</h3>)}</td>
-  </tr>
-
-</table>
-      
-   </div>
-  )
-}
+    return <div className="blog">
+      <ViewPosts />
+    </div>;
+  }
 }
