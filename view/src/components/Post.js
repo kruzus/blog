201235782cac8783668f06ebb48f5294c474021d
Bek <kruzus@live.com>
2019-10-29 20:50:44 -0400

    more work

---
+++ view/src/components/Post.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Post extends Component {
+render(){
+  return(
+  
+   <div>
+       <center>
+           <h1>
+               HI FROM POST
+           </h1>
+       </center>
+   </div>
+  
+  )
+}
+}
