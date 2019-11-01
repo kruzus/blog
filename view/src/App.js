09e8fe8b27001d6019923b99eb74b71eb8980785
Bek <kruzus@live.com>
2019-11-01 01:29:58 -0400

    alpha test on routing

--- view/src/App.js
+++ view/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Switch , Route} from 'react-router-dom';
+
+
 import Header from "./components/Header/Header";
 import Blog from "./components/Blog-container/Blog";
+import Post from './components/Post';
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <main>
+      <Router>
         <Header />
-        <Blog />
-      </main>
+        <Route exact path="/" component={Blog} />
+        <Route exact path="/post" component={Post} />
+        </Router>
     );
   }
 }
